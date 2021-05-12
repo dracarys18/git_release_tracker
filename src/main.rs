a71311c469af7bc118d772991fc71c048402d7ad
dracarys18 <karthihegde010@gmail.com>
2021-05-12 10:14:44 +0530

    Dont have to create new tgclient object for every repo

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,11 @@ async fn main() {
 }
 async fn run(){
     let configs = Config::new();
+    let chat_id = configs.getchatid().to_owned();
+    let token = configs.gettoken().to_owned();
     let track = Tracker::new();
     let client = reqwest::Client::new();
+    let tgclient = TelegramClient::new(&chat_id, &token);
     for i in configs.getrepos(){
         let url = format!("https://api.github.com/repos/{}/releases/latest",i);
         let req = client.get(url)
@@ -30,9 +33,6 @@ async fn run(){
         let status_str = status.as_str();
         let content = req.text().await.ok();
         let json_text = track.parse_resp_json(content,status_str);
-        let chat_id = configs.getchatid().to_owned();
-        let token = configs.gettoken().to_owned();
-        let tgclient = TelegramClient::new(&chat_id, &token);
         let reponame = i.split("/").map(|a| a).collect::<Vec<&str>>()[1];
         match json_text{
             Some(text)=>{
