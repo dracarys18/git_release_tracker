275e396dd8083b3ad343d9a87dbb12036e216095
dracarys18 <karthihegde010@gmail.com>
2021-05-17 11:30:47 +0530

    Panic before making the request if repo isnt in proper format

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,10 @@ async fn run() {
     let client = reqwest::Client::new();
     let tgclient = TelegramClient::new(&chat_id, &token);
     for i in configs.getrepos() {
+        let reponame = match i.split("/").map(|a| a).collect::<Vec<&str>>().get(1){
+            Some(val)=>*val,
+            None=>panic!("The repo name is empty that means you havent defined it in proper format\nThe format is <username>/<reponame>"),
+        };
         let url = format!("https://api.github.com/repos/{}/releases/latest", i);
         let req = client
             .get(url)
@@ -33,10 +37,6 @@ async fn run() {
         let status = &req.status().as_str().to_owned();
         let content = req.text().await.ok();
         let json_text = track.parse_resp_json(content, status);
-        let reponame = match i.split("/").map(|a| a).collect::<Vec<&str>>().get(1){
-            Some(val)=>*val,
-            None=>panic!("The repo name is empty that means you havent defined it in proper format\nThe format is <username>/<reponame>"),
-        };
         match json_text {
             Some(text) => {
                 let filename = i.replace("/", "_");
